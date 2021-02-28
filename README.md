d9911a6cec778fea02a90e3474ce55845e425a22
adriacabeza <adriacabeza@hotmail.com>
2021-02-28 12:04:39 +0100

    Fixing catalan accents encoding and simplifying openALPR script

--- README.md
+++ README.md
@@ -1,17 +1,18 @@
 <h1 align="center"> Control of Access to Natural Spaces</h1>
 
-## Motivation
-Aquest projecte està motivat per [Smart Catalonia]( https://participa.challenge.cat/assemblies/agentsrurals). L'objectiu �s optimitzar i automatizar el proc�s de control d'accés a espais naturals.
-
 [![Open Source Love](https://badges.frapsoft.com/os/v3/open-source.svg?v=103)](https://github.com/ellerbrock/open-source-badges/)
 [![made-with-python](https://img.shields.io/badge/Made%20with-Python-1f425f.svg)](https://www.python.org/)
+
+## Motivation
+Aquest projecte está motivat per [Smart Catalonia]( https://participa.challenge.cat/assemblies/agentsrurals). L'objectiu és optimitzar i automatizar el control d'accés a espais naturals.
+
 
 ## Requisits
 ### Material
 - PiCamera
 - Raspberry Pi
 
-### Ús
+### √ös
 Per a instalar el software, runneja el següent script en una Raspberry amb càmera i internet.
 
 ```bash
@@ -26,12 +27,12 @@ On option és un paràmetre per a escollir la estratègia desitjada:
 Aquesta seria la nostra primera opció ja que és la que requereix dispositius amb menys potència. OpenALPR és una llibreria open-source que detecta matrícules que compleix tots els nostres requísits:
 - L'imatge havia de ser processada localment: enviar streamings de vídeo a un servidor i analitzar-los allà és una solució molt poc eficient, a més és una solució que introdueix a la equació la latència de la xarxa lo qual pot fer que tot vagi molt lent.
 - Ha de funcionar amb imatges de baixa qualitat, la idea és fer servir una càmara d'una raspberry.
-- Ha de ser de lliure ús: en aquest cas la llibreria disposa d'una llicència open source AGPL.
+- Ha de ser de lliure ús: en aquest cas la llibreria disposa d'una llic√®ncia open source AGPL.
 
 L'script que realitzaria aquesta opció el podem trobar a [src/openALPR.py](src/openALPR.py).
 
 ### Opció 2: Tensorflow Lite + OpenALPR
-En el cas de que el nombre de falsos positius fos molt alt, obtaríem per a fer servir també una xarxa neuronal convolucional per a detectar primer als cotxes. En aquest cas hem obtat per un detector molt eficient pensat per a ser executat en dispositius mobils, SSD Mobile Net (https://arxiv.org/pdf/1704.04861.pdf). A més, aprofitaríem el fet de que els cotxes son un objecte prou comú per a utilitzar xarxes que ja estan entrenades (i així disminuir costos).
+En el cas de que el nombre de falsos positius fos molt alt, optaríem per a fer servir també una xarxa neuronal convolucional per a detectar primer als cotxes. En aquest cas hem obtat per un detector molt eficient pensat per a ser executat en dispositius mobils, SSD Mobile Net (https://arxiv.org/pdf/1704.04861.pdf). A més, aprofitaríem el fet de que els cotxes son un objecte prou comú per a utilitzar xarxes que ja estan entrenades (i així disminuir costos).
 Un cop detectat el cotxe, llavors hauríem de córrer OpenALPR per a detectar la matrícula del mateix. En el cas de que OpenALPR no detectés cap matrícula (per diverses causes i.e. només es veu una part del cotxe o una branca la està tapant), continuaríem runnejant el model, el qual eventualment detectaria un frame on el cotxe es pot visualitzar completament.
 
 El framework per a treballar triat ha estat Tensorflow Lite, una libreria d'aprenentatge profund creada per Google específicament per a fer inferència a dispositius de poca capacitat.
--- src/openALPR.py
+++ src/openALPR.py
@@ -1,192 +1,33 @@
-import os
-import csv
-import sys
-import cv2
+
 import time
-import argparse
-import requests
-import importlib.util
 
-import numpy as np
-
-from threading import Thread
-
-import json
+import cv2
 from openalpr import Alpr
+from picamera import PiCamera
+import src.log as log_util
+
 
 alpr = Alpr("eu", "/etc/openalpr/openalpr.conf", "/usr/share/openalpr/runtimedata")
-
-def recognize_license_plate(image_file):
-	results = alpr.recognizefile(image_file)
-	print(json.dumps(results, indent=4))
-	alpr.unload()
-
-assert os.path.exists('information.csv')
-with open('information.csv', newline='') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        client_id = row[0]
-        name = row[1]
-        limit = int(row[2])
-        break
-
-
-def update_number(number):
-    url = 'https://www.lordsantanna.com/'
-    response = request.put(url,{"client_id:{},name:{},number:{}, limit:{}".format(client_id, name, number, limit) )
-    print(response)
-
-
-class VideoStream:
-    """Camera object that controls video streaming from the Picamera"""
-    def __init__(self,resolution=(640,480),framerate=30):
-        # Initialize the PiCamera and the camera image stream
-        self.stream = cv2.VideoCapture(0)
-        ret = self.stream.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
-        ret = self.stream.set(3,resolution[0])
-        ret = self.stream.set(4,resolution[1])
-            
-        # Read first frame from the stream
-        (self.grabbed, self.frame) = self.stream.read()
-
-	# Variable to control when the camera is stopped
-        self.stopped = False
-
-    def start(self):
-	# Start the thread that reads frames from the video stream
-        Thread(target=self.update,args=()).start()
-        return self
-
-    def update(self):
-        # Keep looping indefinitely until the thread is stopped
-        while True:
-            # If the camera is stopped, stop the thread
-            if self.stopped:
-                # Close camera resources
-                self.stream.release()
-                return
-
-            # Otherwise, grab the next frame from the stream
-            (self.grabbed, self.frame) = self.stream.read()
-
-    def read(self):
-	# Return the most recent frame
-        return self.frame
-
-    def stop(self):
-	# Indicate that the camera and thread should be stopped
-        self.stopped = True
-
-
-parser = argparse.ArgumentParser()
-parser.add_argument('--modeldir', help='Folder the .tflite file is located in',
-                    required=True)
-parser.add_argument('--graph', help='Name of the .tflite file, if different than detect.tflite',
-                    default='detect.tflite')
-parser.add_argument('--labels', help='Name of the labelmap file, if different than labelmap.txt',
-                    default='labelmap.txt')
-parser.add_argument('--threshold', help='Minimum confidence threshold for displaying detected objects',
-                    default=0.5)
-parser.add_argument('--mode', help='0 is Camera Mode, 1 is Image mode, 2 is Video Mode')
-parser.add_argument('--resolution', help='Desired webcam resolution in WxH. If the webcam does not support the resolution entered, errors may occur.',
-                    default='1280x720')
-
-args = parser.parse_args()
-
-MODEL_NAME = args.modeldir
-GRAPH_NAME = args.graph
-LABELMAP_NAME = args.labels
-min_conf_threshold = float(args.threshold)
-resW, resH = args.resolution.split('x')
-imW, imH = int(resW), int(resH)
-
-
-# Import TensorFlow libraries
-# If tflite_runtime is installed, import interpreter from tflite_runtime, else import from regular tensorflow
-pkg = importlib.util.find_spec('tflite_runtime')
-if pkg:
-    from tflite_runtime.interpreter import Interpreter
-else:
-    from tensorflow.lite.python.interpreter import Interpreter
-
-
-# Get path to current working directory
-CWD_PATH = os.getcwd()
-
-# Path to .tflite file, which contains the model that is used for object detection
-PATH_TO_CKPT = os.path.join(CWD_PATH,MODEL_NAME,GRAPH_NAME)
-
-# Path to label map file
-PATH_TO_LABELS = os.path.join(CWD_PATH,MODEL_NAME,LABELMAP_NAME)
-
-# Load the label map
-with open(PATH_TO_LABELS, 'r') as f:
-    labels = [line.strip() for line in f.readlines()]
-
-
-# Load the Tensorflow Lite model.
-interpreter = Interpreter(model_path=PATH_TO_CKPT)
-interpreter.allocate_tensors()
-
-# Get model details
-input_details = interpreter.get_input_details()
-output_details = interpreter.get_output_details()
-height = input_details[0]['shape'][1]
-width = input_details[0]['shape'][2]
-
-floating_model = (input_details[0]['dtype'] == np.float32)
-
-input_mean = 127.5
-input_std = 127.5
+camera = PiCamera()
 
 # Initialize frame rate calculation
 frame_rate_calc = 1
 freq = cv2.getTickFrequency()
 
-# Initialize video stream
-videostream = VideoStream(resolution=(imW,imH),framerate=30).start()
-time.sleep(1)
+try:
+    while True:
+        time.sleep(5)
+        camera.capture('/home/pi/latest.jpg')
+        analysis = alpr.recognize_file(
+            "/home/pi/latest.jpg")
+        if len(analysis['results']):
+            number_plate = analysis['results'][0]['plate']
+            log_util.info('Number plate detected: {}'.format(number_plate))
+        # Wait for five seconds
+        time.sleep(5)
 
+except KeyboardInterrupt:
+    log_util.info('Shutting down')
+    alpr.unload()
+    cv2.destroyAllWindows()
 
-while True:
-    time.sleep(5)
-    aux_num = 0
-    # Grab frame from video stream
-    frame1 = videostream.read()
-
-    # Acquire frame and resize to expected shape [1xHxWx3]
-    frame = frame1.copy()
-    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    frame_resized = cv2.resize(frame_rgb, (width, height))
-    input_data = np.expand_dims(frame_resized, axis=0)
-
-    # Normalize pixel values if using a floating model (i.e. if model is non-quantized)
-    if floating_model:
-        input_data = (np.float32(input_data) - input_mean) / input_std
-
-    # Perform the actual detection by running the model with the image as input
-    interpreter.set_tensor(input_details[0]['index'],input_data)
-    interpreter.invoke()
-
-    # Retrieve detection results
-    boxes = interpreter.get_tensor(output_details[0]['index'])[0] # Bounding box coordinates of detected objects
-    classes = interpreter.get_tensor(output_details[1]['index'])[0] # Class index of detected objects
-    scores = interpreter.get_tensor(output_details[2]['index'])[0] # Confidence of detected objects
-
-    # Loop over all detections and draw detection box if confidence is above minimum threshold
-    for i in range(len(scores)):
-        if ((scores[i] > min_conf_threshold) and (scores[i] <= 1.0)) and labels[int(classes[i])] == 'person':
-            aux_num = aux_num +1
-    number_people.append(aux_num)
-    
-    if len(number_people) == 12: # update every minute how many people we can see in the pool
-        update_number(int(sum(number_people)/len(number_people)))
-        aux_num = list()  
-
-    # Press 'q' to quit
-    if cv2.waitKey(1) == ord('q'):
-        break
-
-# Clean up
-cv2.destroyAllWindows()
-videostream.stop()
