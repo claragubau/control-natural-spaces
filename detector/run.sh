0be03e1beb11f69fa908838711f22955a35fd4db
adriacabeza <adriacabeza@hotmail.com>
2021-03-01 17:53:35 +0100

    Creating detector requirements and running script

---
+++ detector/run.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+echo "Download all the dependencies"
+echo "Download utilities"
+apt update
+apt-get install -y --no-install-recommends \
+	git \
+	wget
+echo "Download tensorflow"
+source prepare_tensorflow.sh
+echo "Create environment"
+python3.7 -m venv .env
+source .env/bin/activate
+echo "Install dependencies"
+pip install -r requirements.lock
+echo "Run the detection engine model"
+if [$0 == 'tensorflow']
+then
+    /usr/bin/python3 stc/openALPR_tensorflow.py --modeldir model --graph detect.tflite  --threshold 0.3 --mode 0 --labels model/labelmap.txt
+else
+    /usr/bin/python3 stc/openALPR.py --mode 0
+fi
