a249cd9cc0d46f3be7686d1c29ccc33b458e788a
adriacabeza <adriacabeza@hotmail.com>
2021-03-02 18:53:33 +0100

    System working

--- detector/src/openALPR.py
+++ detector/src/openALPR.py
@@ -4,13 +4,9 @@ import src.log as log_util
 from openalpr import Alpr
 from picamera import PiCamera
 
-alpr = Alpr("eu", "/etc/openalpr/openalpr.conf", "/usr/share/openalpr/runtimedata")
+alpr = Alpr("eu", "/etc/openalpr/openalpr.conf", "/usr/share/openalpr/runtime_data")
 camera = PiCamera()
-
-# Initialize frame rate calculation
-frame_rate_calc = 1
-freq = cv2.getTickFrequency()
-
+previous = None
 try:
     while True:
         time.sleep(5)
@@ -18,7 +14,10 @@ try:
         analysis = alpr.recognize_file("/home/pi/latest.jpg")
         if len(analysis["results"]):
             number_plate = analysis["results"][0]["plate"]
-            log_util.info("Number plate detected: {}".format(number_plate))
+            if previous != number_plate:
+                log_util.info("Number plate detected: {}".format(number_plate))
+                previous = number_plate
+
         # Wait for five seconds
         time.sleep(5)
 
