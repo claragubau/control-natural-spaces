1971bd36e1d0f3175bb65542aa76a36a0688ee2c
adriacabeza <adriacabeza@hotmail.com>
2021-03-14 09:18:51 +0100

    Final tests with videos, if we wanted a faster version we'd need Threads

--- detector/src/test_openALPR_video.py
+++ detector/src/test_openALPR_video.py
@@ -1,48 +1,56 @@
 import argparse
-import time
 
 import cv2
+import time
 import numpy as np
-import src.log as log_util
 from openalpr import Alpr
 from src.request_utils import call_api
 
 parser = argparse.ArgumentParser()
-parser.add_argument(
-    "--resolution",
-    help="Desired webcam resolution in WxH. If the webcam does not support the resolution entered, errors may occur.",
-    default="1280x720",
-)
 parser.add_argument("--video", help="Path to the video, just for testing purposes", default=0)
 args = parser.parse_args()
 
 alpr = Alpr("eu", "/usr/local/share/openalpr/config/openalpr.defaults.conf", "openalpr/runtime_data/")
 
-resW, resH = args.resolution.split("x")
-imW, imH = int(resW), int(resH)
-
 # Initialize video stream
 video_stream = cv2.VideoCapture(args.video)
+frame_rate = 30
+
 
 def run():
     try:
-        while video_stream.isOpened():
-            # Grab frame from video stream
-            _, frame = video_stream.read()
-            if frame is not None:
-                # cv2.imshow("Image", frame)
-                # if cv2.waitKey(1) & 0xFF == ord('q'):
-                #      break
+        prev = 0
+        while True:
+            time_elapsed = time.time() - prev
+            if time_elapsed > 1. / frame_rate:
+                _, frame = video_stream.read()
+                prev = time.time()
                 analysis = alpr.recognize_ndarray(frame)
                 if len(analysis["results"]):
                     number_plate = analysis["results"][0]["plate"]
-                    cv2.imwrite("./latest.jpg", frame)
-                    response = call_api("./latest.jpg", number_plate)
-                    print(response)
+                    coordinates = [x for x in
+                                   [analysis["results"][0]["coordinates"][0]["x"],
+                                    analysis["results"][0]["coordinates"][0]["y"],
+                                    analysis["results"][0]["coordinates"][2]["x"],
+                                    analysis["results"][0]["coordinates"][2]["y"]]
+                                   ]
+                    font = cv2.FONT_HERSHEY_SIMPLEX
+                    font_scale = 3
+                    (text_width, text_height) = cv2.getTextSize(number_plate, font, fontScale=font_scale, thickness=1)[0]
+                    box_coords = ((coordinates[2], coordinates[3]), (coordinates[2] + text_width + 2, coordinates[3] - text_height - 2))
+                    cv2.rectangle(frame, box_coords[0], box_coords[1], (255, 255, 255), cv2.FILLED)
+                    cv2.putText(frame, number_plate, (coordinates[2], coordinates[3]), cv2.FONT_HERSHEY_SIMPLEX, font_scale, (240, 0, 0), 3)
+                    frame_resized = cv2.resize(frame, (720, 480))
+                    cv2.imshow("Test", frame)
+                    cv2.waitKey(10000)
+                    #cv2.imwrite("./latest_{}.jpg".format(time_elapsed), frame)
+                    #response = call_api("./latest.jpg", number_plate)
+                    #print(response)
         video_stream.release()
         cv2.destroyAllWindows()
     except Exception as e:
         print('Exception happened: {}'.format(e))
+
 
 if __name__ == "__main__":
     run()
